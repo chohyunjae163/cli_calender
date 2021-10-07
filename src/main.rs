5aa40d522bbe8a637ab3fbf62162cc96fd31c63e
chohyunjae <chohyunjae163@gmail.com>
2021-10-07 23:07:50 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// Declare array, initialize all values, compiler infers length = 7
+
+
+fn main() {
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    println!("Hello,  rust world!");
+}
