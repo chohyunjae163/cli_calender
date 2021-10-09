2d3cd239cec75b3c34a4476ee530c787b23b1fa8
chohyunjae <chohyunjae163@gmail.com>
2021-10-09 15:44:37 +0900

    implementing config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,37 @@
-
+use std::env;
+use std::process;
 
 fn main() {
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    println!("Hello,  rust world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments:\n {}", err);
+        process::exit(1);
+    });
+
+    run(config);
+}
+
+struct Config {
+    date: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        //what if a user writes no input? or more than one input?
+        if args.len() != 2 {
+            return Err("this program requires only one argument, date\n format: YEAR-MONTH-DAY");
+        }
+        //check the validity of the input.
+        let input = &args[1];
+        for c in input.chars() {
+            let year = c;
+        }
+        let date = args[1].clone();
+        println!("parsed date: {}", date);
+        Ok(Config { date })
+    }
+}
+
+fn run(config: Config) {
+    println!("running...on date: {}", config.date);
 }
