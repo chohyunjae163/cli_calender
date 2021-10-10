3578285cc28c133e8d5d212547448dda7ec6f661
chohyunjae <chohyunjae163@gmail.com>
2021-10-10 14:16:09 +0900

    added fn leap_year, fn parse_date

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments:\n {}", err);
-        println!("this program requires one argument\n format: YEAR-MONTH-DAY");
+        println!("Config new error: {}", err);
+        println!("this program requires one argument - format: YEAR-MONTH-DAY");
         process::exit(1);
     });
 
-    println!("{}", config.date)
+    println!("{}", config.date);
+    //let date = config.date;
 }
 
 struct Config {
@@ -28,15 +29,15 @@ impl Config {
         let date = get_date(input);
         match date {
             Ok(v) => Ok(Config { date: v }),
-            Err(e) => return Err(e),
+            Err(e) => Err(e),
         }
     }
 }
 
 struct Date {
-    year: String,
-    month: String,
-    day: String,
+    year: u32,
+    month: u8,
+    day: u8,
 }
 
 impl fmt::Display for Date {
@@ -46,33 +47,69 @@ impl fmt::Display for Date {
 }
 
 fn get_date(arg: &String) -> Result<Date, &'static str> {
-    let mut date: Vec<String> = vec!(String::from(""),String::from(""),String::from(""));
+    let mut strs: Vec<String> = vec![String::from(""), String::from(""), String::from("")];
     let mut index = 0;
+    //expected string format year-month-day.
     for c in arg.chars() {
         if c == '-' {
             index += 1;
             continue;
-        } 
-        date[index].push(c);
+        }
+        strs[index].push(c);
     }
-    let year = date[0].clone();
-    if is_numeric(year.parse::<i32>()) == false {
-        return Err("the year input seems to be wrong....");
+    let result = parse_date(&strs[0], &strs[1], &strs[2]);
+    match result {
+        Ok(date) => Ok(date),
+        Err(e) => Err(e),
     }
-    let month = date[1].clone();
-    if is_numeric(month.parse::<i32>()) == false {
-        return Err("the month input seems to be wrong....");
+}
+
+fn parse_date(yy: &String, mm: &String, dd: &String) -> Result<Date, &'static str> {
+    let year: u32;
+    let month: u8;
+    let day: u8;
+    let result = yy.parse::<u32>();
+    match result {
+        Ok(v) => {
+            if v < 1 {
+                return Err("the year starts from 1 A.D.");
+            }
+            year = v
+        }
+        Err(_e) => return Err("failed parsing in32 from a year"),
     }
-    let day = date[2].clone();
-    if is_numeric(day.parse::<i32>()) == false {
-        return Err("the day input seems to be wrong....");
+    let result = mm.parse::<u8>();
+    match result {
+        Ok(v) => {
+            if v < 1 || v > 12 {
+                return Err("a month should be within 1-12");
+            }
+            month = v
+        }
+        Err(_e) => return Err("failed parsing in32 from a month"),
     }
+    let result = dd.parse::<u8>();
+    match result {
+        Ok(v) => {
+            if v < 1 || v > 31 {
+                return Err("day should be within 1-31");
+            }
+            if month == 2 {
+                if v == 30 || v == 31 {
+                    return Err("30 or 31 does not exist in Feb");
+                }
+                if v == 29 && is_leap_year(year) == false {
+                    return Err("not a leap year");
+                }
+            }
+            day = v
+        }
+        Err(_e) => return Err("failed parsing in32 from a day"),
+    }
+
     Ok(Date { year, month, day })
 }
 
-fn is_numeric(num: Result<i32, std::num::ParseIntError>) -> bool {
-    match num {
-        Ok(_val) => true,
-        Err(_why) => false,
-    }
+fn is_leap_year(year: u32) -> bool {
+    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
 }
