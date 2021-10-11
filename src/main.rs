adf2c000137329260334e16df7111ca64c19e045
hyunjae cho <chohyunjae163@gmail.com>
2021-10-11 21:17:17 +0900

    added a func calc_day

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,31 @@ fn main() {
     println!("GREGORIAN CALENDER");
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Config new error: {}", err);
-        println!("this program requires one argument - format: YEAR-MONTH-DAY");
+        eprintln!("config error: {}", err);
+        eprintln!("this program requires one argument - format: YEAR-MONTH-DAY");
         process::exit(1);
     });
 
-    println!("config date: {}", config.date);
     let date = config.date;
-    date.year;
-    date.month;
-    date.day;
-
-    let leap_count = get_leap_year_count(date.year);
-    println!("leap year count: {}", leap_count);
+    dbg!(&date);
+    match calc_day(&date) {
+        Ok(day) => println!("{}. The day of the week was {}", date, DAYS[day as usize]),
+        Err(e) => eprintln!("{}", e),
+    }
 }
+
+//the Gregorian calendar is adopted in 1582
+const GREGORIAN_CALENDAR: u32 = 1582;
+//Jan. 1st of the Gregorian calendar is Thursday.
+const DAYS: [&'static str; 7] = [
+    "THURSDAY",
+    "FRIDAY",
+    "SATURDAY",
+    "SUNDAY",
+    "MONDAY",
+    "TUESDAY",
+    "WEDNESDAY",
+];
 
 struct Config {
     date: Date,
@@ -41,6 +52,7 @@ impl Config {
     }
 }
 
+#[derive(Debug)]
 struct Date {
     year: u32,
     month: u32,
@@ -49,7 +61,7 @@ struct Date {
 
 impl fmt::Display for Date {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "INPUT DATE : {} {} {}", self.year, self.month, self.day)
+        write!(f, "Date : {}-{}-{} ", self.year, self.month, self.day)
     }
 }
 
@@ -78,8 +90,8 @@ fn parse_date(yy: &String, mm: &String, dd: &String) -> Result<Date, &'static st
     let result = yy.parse::<u32>();
     match result {
         Ok(v) => {
-            if v < 1 {
-                return Err("the year starts from 1 A.D.");
+            if v < GREGORIAN_CALENDAR {
+                return Err("The Gregorian calendar was introduced in 1582.");
             }
             year = v
         }
@@ -118,14 +130,48 @@ fn parse_date(yy: &String, mm: &String, dd: &String) -> Result<Date, &'static st
 }
 
 fn is_leap_year(year: u32) -> bool {
-    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+    let leap_year = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    dbg!(leap_year);
+    leap_year
 }
 
-fn get_leap_year_count(year: u32) -> u32 {
-    //let months: [i32; 12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    //let months_leap_year: [i32; 12] = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    //let total = ((year - 1) * 365) + ((month - 1) * 30) + day;
+fn get_leap_year_count(year: u32) -> Result<u32, &'static str> {
+    if year < GREGORIAN_CALENDAR {
+        return Err("the input year is smaller than the Gregorian calendar");
+    }
 
-    let count = (year / 4) - (year / 100);
-    count
+    let pre_gregorian_leaps =
+        (GREGORIAN_CALENDAR / 4) - (GREGORIAN_CALENDAR / 100) + (GREGORIAN_CALENDAR / 400);
+
+    let leap_year_count = (year / 4) - (year / 100) + (year / 400) - pre_gregorian_leaps;
+    dbg!(leap_year_count);
+    Ok(leap_year_count)
+}
+
+fn calc_day(date: &Date) -> Result<u32, &'static str> {
+    const YEAR: u32 = 365;
+    let mut days = 0;
+    if date.month > 1 {
+        //DECEMBER is not needed.
+        const MONTHS: [u32; 11] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30];
+        const MONTHS_LEAP_YEAR: [u32; 11] = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30];
+        let month_minus_one = (date.month - 1) as usize;
+        if is_leap_year(date.year) {
+            for d in &MONTHS_LEAP_YEAR[0..month_minus_one] {
+                days += d;
+            }
+        } else {
+            for d in &MONTHS[0..month_minus_one] {
+                days += d;
+            }
+        }
+    }
+
+    match get_leap_year_count(date.year) {
+        Ok(leap_year_count) => {
+            days += (date.year - GREGORIAN_CALENDAR) * YEAR + date.day + leap_year_count
+        }
+        Err(e) => return Err(e),
+    }
+    Ok(days % 7)
 }
