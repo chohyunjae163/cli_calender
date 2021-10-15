60f37d35d977efb19497ed14d45207e536f3b0b5
chohyunjae <chohyunjae163@gmail.com>
2021-10-15 13:49:29 +0900

    fixed a leap year bug

--- src/date.rs
+++ src/date.rs
@@ -1,8 +1,9 @@
 use std::convert::TryInto;
 use std::fmt;
+
 //the Gregorian calendar is adopted in 1582
 const GREGORIAN_CALENDAR: u32 = 1582;
-//Jan. 1st of the Gregorian calendar is Thursday.
+//Jan. 1st of the Gregorian calendar is Thursday in 1582.
 const DAYS: [&'static str; 7] = [
     "THURSDAY",
     "FRIDAY",
@@ -24,6 +25,37 @@ impl fmt::Display for Date {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Date : {}-{}-{}", self.year, self.month, self.day)
     }
+}
+
+pub fn run() {
+    //print hello calendar title ascii code
+    print_title();
+
+    loop {
+        let mut line = String::new();
+        println!("Enter a date (YEAR-MONTH-DATE) or press Q to exit:");
+        std::io::stdin().read_line(&mut line).unwrap();
+        line = line.trim().to_string();
+        // if a user wants to quit, exit.
+        if line == "q" || line == "Q" {
+            println!("goodbye!");
+            return;
+        }
+        //parse date from that input.or else go back and ask for a date again.
+        match parse_date(&line) {
+            Ok(v) => {
+                let day = calc_day(&v);
+                println!("{}. The day of the week is {}", v, DAYS[day as usize]);
+                break;
+            }
+            Err(e) => eprintln!("{}", e),
+        };
+    }
+}
+
+fn print_title() {
+    let bytes = include_bytes!("../asset/title.txt");
+    println!("{}", String::from_utf8_lossy(bytes));
 }
 
 fn parse_date(arg: &String) -> Result<Date, &'static str> {
@@ -112,38 +144,12 @@ fn calc_day(date: &Date) -> u32 {
         }
     }
     let leap_year_count = get_leap_year_count(date.year);
-    const YEAR: u32 = 365;
-    days += (date.year - GREGORIAN_CALENDAR) * YEAR + date.day + leap_year_count;
-    days % 7
-}
-
-fn print_title() {
-    let bytes = include_bytes!("../asset/title.txt");
-    println!("{}", String::from_utf8_lossy(bytes));
-}
-
-pub fn run() {
-    //print hello calendar title
-    print_title();
-
-    loop {
-        let mut line = String::new();
-        println!("Enter a date (YEAR-MONTH-DATE) or press Q to exit:");
-        std::io::stdin().read_line(&mut line).unwrap();
-        line = line.trim().to_string();
-        // if a user wants to quit, exit.
-        if line == "q" || line == "Q" {
-            println!("goodbye!");
-            return;
-        }
-        //parse date from that input.or else go back and ask for a date again.
-        match parse_date(&line) {
-            Ok(v) => {
-                let day = calc_day(&v);
-                println!("{}. The day of the week is {}", v, DAYS[day as usize]);
-                break;
-            }
-            Err(e) => eprintln!("{}", e),
-        };
+    let mut extra_days_for_leap_years = leap_year_count;
+    //if the year is leap year, minus one.
+    if is_leap_year(date.year) {
+        extra_days_for_leap_years -= 1;
     }
+    const YEAR: u32 = 365;
+    days += (date.year - GREGORIAN_CALENDAR) * YEAR + date.day + extra_days_for_leap_years;
+    days % 7
 }
